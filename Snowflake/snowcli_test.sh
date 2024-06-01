bb3b5f237f511f95576f9ab44ebf41a57688448d
Yin-Ting Chou <choux130@umn.edu>
2024-06-01 17:59:04 -0400

    container for connecting to snowflake

---
+++ Snowflake/snowcli_test.sh
@@ -0,0 +1,4 @@
+snow connection test --connection horizon-aws-provider
+snow connection test --connection horizon-aws-consumer
+snow connection test --connection horizon-azure-consumer
+snow connection test 
